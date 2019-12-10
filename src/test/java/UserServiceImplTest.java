29e8b466ab007f439f4b14bab9e017bbff882850
Sharique <mdanwersharique@gmail.com>
2019-12-10 17:27:33 +0100

    test to verify user password

---
+++ src/test/java/UserServiceImplTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
+public class UserServiceImplTest {
+
+    @Test
+    public void verifyUserGetsThePassword() throws Exception {
+        //arrange
+        User user = mock(User.class);
+        UserDAO userDao = mock(UserDAO.class);
+        SecurityService securityService = mock(SecurityService.class);
+        UserServiceImpl userService = new UserServiceImpl(userDao, securityService);
+        when(user.getPassword()).thenReturn("testPassword");
+        //act
+        userService.assignPassword(user);
+        //assert
+        verify(user).setPassword(any());
+        verify(user).getPassword();
+        verify(userDao).updateUser(user);
+
+
+    }
+}
