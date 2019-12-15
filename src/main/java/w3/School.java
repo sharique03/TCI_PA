d3ecb329f38a2326b111e024146dff10b4e583f0
Sharique <mdanwersharique@gmail.com>
2019-12-15 20:30:14 +0100

    W3 schoolNameAndOpenningDateShouldBeSetAtCreation refactor

--- src/main/java/w3/School.java
+++ src/main/java/w3/School.java
@@ -4,16 +4,21 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class School {
+    private String schoolName;
+    private LocalDate opennningDate;
+    private List<Course>courseList;
 
     public School(String schoolName, LocalDate openningDate, List<Course> courseList) {
-
+        this.schoolName=schoolName;
+        this.opennningDate=openningDate;
+        this.courseList=courseList;
     }
 
     public LocalDate getOpenningDate() {
-        return LocalDate.parse("2019-03-31");
+        return this.opennningDate;
     }
 
     public String getSchoolName() {
-        return "Fontys";
+        return this.schoolName;
     }
 }
