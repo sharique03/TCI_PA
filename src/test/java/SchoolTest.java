ed798309fa85192453b79138a32a33dfa2a1b066
Sharique <mdanwersharique@gmail.com>
2019-12-15 19:30:37 +0100

    W3 addCourseEndDateAfterStartDate test added with assert fail

--- src/test/java/SchoolTest.java
+++ src/test/java/SchoolTest.java
@@ -1,17 +1,18 @@
 import org.junit.Test;
+import org.junit.Assert;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 public class SchoolTest {
 
     @Test
-    public void addCourseEndDateAfterStartDate()
-    {
+    public void addCourseEndDateAfterStartDate(){
+        //arrange
         final LocalDate startDate = LocalDate.parse("2019-03-31");
         final LocalDate endDate = LocalDate.parse("2019-04-05");
+        //act
         Course course=new Course("TCICourseName",startDate,endDate);
-
+        //assert
+        Assert.assertNotNull(course);
     }
 }
